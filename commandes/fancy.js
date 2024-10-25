fdee28dbeef4767ae97ef52e6db73125953b5ab4
ELIJAH-Ombogo <elijahombogo2004@gmail.com>
2024-10-25 18:57:24 +0300

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "✍️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 YESSER BOY\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('YESSER😎_MD ', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 GLEN BOY\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('GLEN😎_MD ', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
