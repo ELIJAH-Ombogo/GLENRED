ebc0914655e8a380d028834f3ec576c1dcfe8476
ELIJAH-Ombogo <elijahombogo2004@gmail.com>
2024-10-25 23:33:13 +0300

    Update mesfonctions.js

--- framework/mesfonctions.js
+++ framework/mesfonctions.js
@@ -9,7 +9,7 @@ const baileys_1 = require("@whiskeysockets/baileys");
 const fs = require('fs-extra');
 const util = require('util');
 let { listall } = require('./stylish-font');
-/*_________by YESSER tech
+/*_________by glen tech
 
 fonction zJson:
 récupère un objet json
@@ -32,7 +32,7 @@ module.exports.genererNomFichier = async (extension) => {
 /** ************ */
 module.exports.stick = async (buffer, author) => {
     var sticker = new Sticker(buffer, {
-        pack: 'YESSER-MD',
+        pack: 'GLEN-MD',
         author: author,
         type: StickerTypes.FULL,
         categories: ['🤩', '🎉'],
